13964a374478ab748295b201330fabdabc96eb0c
Mohamed.bouoni <mohamed.bouoni@horizon-tech.tn>
2026-05-30 18:45:33 +0100

    Fix WAF crash loop caused by non-idempotent entrypoint script appending duplicate rules

--- waf/99-exclusions.sh
+++ waf/99-exclusions.sh
@@ -2,7 +2,7 @@
 
 # Exclude PostHog cookies from SQL injection rule
 echo 'SecRuleUpdateTargetById 942290 "!REQUEST_COOKIES:/^ph_/"' \
-  >> /etc/modsecurity.d/owasp-crs/rules/REQUEST-999-COMMON-EXCEPTIONS-AFTER.conf
+  > /etc/modsecurity.d/owasp-crs/rules/REQUEST-999-COMMON-EXCEPTIONS-AFTER.conf
 
 # Exclude Vite dev assets from .js extension restriction (rule 920440)
 echo 'SecRule REQUEST_URI "@beginsWith /node_modules/" "id:1001,phase:1,pass,nolog,ctl:ruleRemoveById=920440"' \
